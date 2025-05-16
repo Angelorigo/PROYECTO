d76c224104630889be326df7c656c7537fa4a7f1
angelorigo <angel.rodrigo.servin@gmail.com>
2025-05-16 17:18:56 -0600

    Séptimo avance (conteo de OE]

--- MERGESORT.cpp
+++ MERGESORT.cpp
@@ -9,57 +9,57 @@
 using namespace std;
 
 void MEZCLA(int A[], int n1, int B[], int n2, int C[]) {  
-	int i=0, j=0, k=0;
-	while (i<n1 && j<n2){
-		if (A[i] <= B[j]){
-			C[k] = A[i];
-			i++;}
+	int i=0, j=0, k=0;	// 3 OE
+	while (i<n1 && j<n2){	// 3 OE
+		if (A[i] <= B[j]){	// 3 OE
+			C[k] = A[i];	// 3 OE
+			i++;}	// 2 OE
 		else {
-			C[k] = B[j];
-			j++;}
-		k++;}
-	while (i<n1){
-		C[k] = A[i];
-		i++;
-		k++;}
-	while (j<n2){
-		C[k] = B[j];
-		j++;
-		k++;}}
+			C[k] = B[j];	// 3 OE
+			j++;}	// 2 OE
+		k++;}	// 2 OE
+	while (i<n1){	// 1 OE
+		C[k] = A[i];	// 3 OE
+		i++;	// 2 OE
+		k++;}	// 2 OE
+	while (j<n2){	// 1 OE
+		C[k] = B[j];	// 3 OE
+		j++;	// 2 OE
+		k++;}}	// 2 OE
 
 void MERGESORT(int A[], int ini, int fin){
-	if (ini<fin){
-		int mitad = (ini + fin)/2;
-		MERGESORT(A, ini, mitad);
-		MERGESORT(A, mitad+1, fin);
-		int n1 = mitad-ini+1;
-		int n2 = fin-mitad;
-		int* izq = new int [n1];
-		int* der = new int [n2];
-		int* C = new int [n1+n2];
+	if (ini<fin){	// 1 OE
+		int mitad = (ini + fin)/2;	// 3 OE
+		MERGESORT(A, ini, mitad);	// 1 OE
+		MERGESORT(A, mitad+1, fin);	// 1 OE
+		int n1 = mitad-ini+1;	// 3 OE
+		int n2 = fin-mitad;	// 2 OE
+		int* izq = new int [n1];	// 1 OE
+		int* der = new int [n2];	// 1 OE
+		int* C = new int [n1+n2];	// 2 OE
 
-		for (int i = 0; i<n1; i++)
-			izq[i] = A[ini+i];
-		for (int i = 0; i<n2; i++)
-			der[i] = A[mitad+1+i];
-		MEZCLA(izq, n1, der, n2, C);
-		for (int i = 0; i<n1+n2; i++)
-			A[ini+i] = C[i];
-		delete[] izq;
-		delete[] der;
-		delete[] C;}}
+		for (int i = 0; i<n1; i++)	// 4 OE
+			izq[i] = A[ini+i];	// 3 OE
+		for (int i = 0; i<n2; i++)	// 4 OE
+			der[i] = A[mitad+1+i];	// 3 OE
+		MEZCLA(izq, n1, der, n2, C);	// 1 OE
+		for (int i = 0; i<n1+n2; i++)	// 4 OE
+			A[ini+i] = C[i];	// 3 OE
+		delete[] izq;	// 1 OE
+		delete[] der;	// 1 OE
+		delete[] C;}}	// 1 OE
 
 void ImpArray(int A[], int n){
-	for (int i = 0; i<n; i++)
-		cout << A[i] << " ";
-		cout << endl;}
+	for (int i = 0; i<n; i++)	// 4 OE
+		cout << A[i] << " ";	// 3 OE
+		cout << endl;}	// 1 OE
 
 int main(){
-	int A[] = {5, 8, 10, 20, 50, 41, 9, 26, 0, 4};
-	int n = sizeof(A)/sizeof(A[0]);
-	cout << "Arreglo original: \n";
-	ImpArray(A, n);
-	MERGESORT(A, 0, n-1);
-	cout << "Arreglo ordenado con MERGESORT: \n";
-	ImpArray(A, n);
-	return 0;}
+	int A[] = {5, 8, 10, 20, 50, 41, 9, 26, 0, 4};	// 1 OE
+	int n = sizeof(A)/sizeof(A[0]);	// 2 OE
+	cout << "Arreglo original: \n";	// 1 OE
+	ImpArray(A, n);	// 1 OE
+	MERGESORT(A, 0, n-1);	// 1 OE
+	cout << "Arreglo ordenado con MERGESORT: \n";	// 1 OE
+	ImpArray(A, n);	// 1 OE
+	return 0;}	// 1 OE
