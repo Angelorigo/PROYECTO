0a73f9ed3c94a7cea185857118dd2f7fd34d773b
angelorigo <angel.rodrigo.servin@gmail.com>
2025-05-16 19:13:57 -0600

    XII avance

--- AVV.cpp
+++ AVV.cpp
@@ -8,51 +8,51 @@
 #include <climits>
 using namespace std;
 
-const int NumNodos = 4;
+const int NumNodos = 4;	// 1 OE
 
-int costo[NumNodos][NumNodos] = {
+int costo[NumNodos][NumNodos] = {	// 3 OE
 	{0, 10, 15, 20},
 	{10, 0, 35, 25},
 	{15, 35, 0, 30},
 	{20, 25, 30, 0}};
 
-bool nodosasignados[NumNodos];
+bool nodosasignados[NumNodos];	
 int trayectoria[NumNodos+1];
 
 int buscamejornodo(int nodoactual){
-	int mejorcosto = INT_MAX;
-	int mejornodo = -1;
-	for (int nodo = 0; nodo < NumNodos; nodo++){
-		if (!nodosasignados[nodo] && costo[nodoactual][nodo] < mejorcosto){
-			mejorcosto = costo[nodoactual][nodo];
-			mejornodo = nodo;}}
-	return mejornodo;}
+	int mejorcosto = INT_MAX;	// 2 OE
+	int mejornodo = -1;	// 1 OE
+	for (int nodo = 0; nodo < NumNodos; nodo++){	// 4 OE
+		if (!nodosasignados[nodo] && costo[nodoactual][nodo] < mejorcosto){	// 6 OE
+			mejorcosto = costo[nodoactual][nodo];	// 3 OE
+			mejornodo = nodo;}}	// 1 OE
+	return mejornodo;}	// 1 OE
 
 int calculacosto(){
-	int suma = 0;
-	for (int i = 0; i<NumNodos; i++){
-		suma = suma + costo[trayectoria[i]][trayectoria[i+1]];}
-	return suma;}
+	int suma = 0;	// 1 OE
+	for (int i = 0; i<NumNodos; i++){	// 4 OE
+		suma = suma + costo[trayectoria[i]][trayectoria[i+1]];}	// 7 OE
+	return suma;}	// 1 OE
 
 void avv(){
-	for (int i = 0; i<NumNodos; i++){
-		nodosasignados[i] = false;}
-	int nodoactual = 0;
-	for (int i = 0; i<NumNodos; i++){
-		nodosasignados[nodoactual] = true;
-		trayectoria[i] = nodoactual;
-		int nodosiguiente = buscamejornodo(nodoactual);
-		nodoactual = nodosiguiente;}
-	trayectoria[NumNodos] = 0;
-	cout << "Trayectoria mímima: ";
-	for (int i = 0; i <= NumNodos; i++){
-		cout << char('A' + trayectoria[i]);
-		if (i < NumNodos) 
-			cout << " -> ";}
-	cout << endl;
-	int costototal = calculacosto();
-	cout << "El costo total del viaje es de $" << costototal << endl;}
+	for (int i = 0; i<NumNodos; i++){ // 4 OE
+		nodosasignados[i] = false;}	// 3 OE
+	int nodoactual = 0;	// 1 OE
+	for (int i = 0; i<NumNodos; i++){	// 4 OE
+		nodosasignados[nodoactual] = true;	// 3 OE
+		trayectoria[i] = nodoactual;	// 3 OE
+		int nodosiguiente = buscamejornodo(nodoactual);	// 2 OE
+		nodoactual = nodosiguiente;}	// 1 OE
+	trayectoria[NumNodos] = 0;	// 2 OE
+	cout << "Trayectoria mímima: ";	// 1 OE
+	for (int i = 0; i <= NumNodos; i++){	// 4 OE
+		cout << char('A' + trayectoria[i]);	// 3 OE
+		if (i < NumNodos)	// 1 OE
+			cout << " -> ";}	// 1 OE
+	cout << endl;	// 1 OE
+	int costototal = calculacosto();	// 2 OE
+	cout << "El costo total del viaje es de $" << costototal << endl;}	// 3 OE
 
 int main(){
-	avv();
-	return 0;}
+	avv();	// 1 OE
+	return 0;}	// 1 OE
